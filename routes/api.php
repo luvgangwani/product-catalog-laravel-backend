94ba88e783406ffd9c13a52274b0b933820fbfa5
Luv Gangwani <luvgangwani>
2020-04-29 23:31:12 +1000

    - Adding Users API resources and controller files - Adding endpoints to get all users, users by id and users by username

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Users
+
+// Route::get('/users', 'UsersController@index');
+
+Route::apiResource('users', 'UsersController');
+
+Route::post('/getUserById', 'UsersController@getUserById');
+
+Route::post('/getUserByUsername', 'UsersController@getUserByUserName');
